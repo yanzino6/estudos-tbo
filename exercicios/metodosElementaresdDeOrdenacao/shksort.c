ab9a44fe250080293b00b3d52829133897a78a8d
yanzino6 <yansimmer@outlook.com>
2026-09-17 10:08:16 -0300

    add: shaker sort

--- exercicios/metodosElementaresdDeOrdenacao/shksort.c
+++ exercicios/metodosElementaresdDeOrdenacao/shksort.c
@@ -2,5 +2,34 @@
 
 void sort (Item *a, int lo, int hi )
 {
+    int sw=1;
+
+    while (sw==1)
+    {
+        sw=0;
+        for (int i = lo; i < hi; i++)
+        {
+            if (a[i]>a[i+1])
+            {
+                exch(a[i],a[i+1]);
+                sw=1;
+            }
+        }
+        if (sw==0)
+            {
+                break;
+            }
+        for (int i = hi; i > lo; i--)
+        {
+            if (a[i]<a[i-1])
+            {
+                exch(a[i],a[i-1]);
+                sw=1;
+            }
+            
+        }
+        
+    }
+    
     
 }
